e14e490d4229b87f4921f9d72464d810da97029c
Jerry Quintero <jerryq27@gmail.com>
2022-04-10 23:02:22 -0500

    Collectibles are being generated!

--- scripts/generate_collection.py
+++ scripts/generate_collection.py
@@ -1,4 +1,7 @@
+from operator import contains
 import os
+import random
+from turtle import back
 from PIL import Image
 
 # 5 layers x 5 options (color, pattern, background, gloss position, (rare) has crack)
@@ -15,31 +18,87 @@ egg_metadata = {
 
 def generate_collection():
     # [0-4][0-4][0-4][0-4?]
-    for i in range(COLLECTION_SIZE):
-        print(i)
+    print('Generating collection..')
+    for i in range(10):
+        print('Calculating unique sequence..')
+        sequence = calculate_sequence()
+        print('Getting collection layers..')
+        images = get_images(sequence)
+        print('Creating collectible..')
+        create_collectible(sequence, images)
+        print()
 
 
 def calculate_sequence():
-    pass
+    unique = False
+
+    while not unique:
+        # #,###
+        background = random.randrange(1, 6) * 10 ** 3
+        color = random.randrange(1, 6) * 10 ** 2
+        pattern = random.randrange(1, 6) * 10
+        # 25% chance of getting an egg with a crack.
+        crack = determine_trait_pattern() if random.randrange(1, 101) >= 75 else 0
+
+        sequence = background + color + pattern + crack
+
+        if sequence in dragon_eggs:
+            continue
+        else:
+            unique = True
+
+            dragon_eggs[sequence] = str(sequence)
+            return dragon_eggs[sequence]
 
 
-def create_collectible(sequence):
+# If this rare trait is going to be generated, determine which pattern.
+
+
+def determine_trait_pattern():
+    pattern_num = random.randint(1, 101)
+
+    # More common pattern 25% chance.
+    if pattern_num >= 25:
+        return 1
+    elif pattern_num > 25 and pattern_num <= 50:
+        return 2
+    # Less common pattern 20% chance.
+    elif pattern_num > 50 and pattern_num <= 70:
+        return 3
+    elif pattern_num > 70 and pattern_num <= 90:
+        return 4
+    # Rarest pattern 10% chance.
+    else:
+        return 5
+
+
+def get_images(sequence):
     img_path = os.path.join(os.getcwd(), 'metadata', 'img')
-    collectibles_path = os.path.join(img_path, 'collectibles')
+
+    background = os.path.join(img_path, f'background-{sequence[0]}.png')
+    color = os.path.join(img_path, f'color-{sequence[1]}.png')
+    pattern = os.path.join(img_path, f'pattern-{sequence[2]}.png')
+    crack = None if sequence[3] == '0' else os.path.join(
+        img_path, f'crack-{sequence[3]}.png')
+
+    images = [background, color, pattern, crack]
+    return images if crack else images[:-1]
+
+
+def create_collectible(sequence, images):
+    collectibles_path = os.path.join(
+        os.getcwd(), 'metadata', 'img', 'collectibles')
     if not os.path.exists(collectibles_path):
         os.mkdir(collectibles_path)
 
-    # layers = [Image.open(x) for x in [
-    #     os.path.join(img_path, 'base.png'),
-    #     os.path.join(img_path, 'pattern.png')
-    # ]]
+    layers = [Image.open(x) for x in images]
 
-    # print('Generating NFT...')
-    # collectible = layers[0]
-    # for layer in layers[1:]:
-    #     collectible.paste(layer, (0, 0), layer)
+    print('Generating NFT...')
+    collectible = layers[0]
+    for layer in layers[1:]:
+        collectible.paste(layer, (0, 0), layer)
 
-    # collectible.save(os.path.join(collectibles_path, 'collectible.png'), 'PNG')
+    collectible.save(os.path.join(collectibles_path, f'{sequence}.png'), 'PNG')
     print('Done!')
 
 
