2f52e6f4cf6a49e6aa12c5bad4190eee7be961f2
Jerry Quintero <jerryq27@gmail.com>
2022-04-16 19:04:20 -0500

    Added render condition if there is no browser wallet.

--- dragoneggs-frontend/src/App.tsx
+++ dragoneggs-frontend/src/App.tsx
@@ -1,5 +1,11 @@
 import React, { useState } from 'react';
-// import { makeStyles } from '@material-ui/core';
+import {
+    Button,
+    Card,
+    CardContent,
+    CardActions,
+    Typography,
+} from '@material-ui/core';
 import { makeStyles, createStyles, Theme } from '@material-ui/core/styles';
 import Header from './components/Header';
 import Body from './components/Body';
@@ -7,24 +13,40 @@ import Body from './components/Body';
 const useStyles = makeStyles((theme: Theme) => createStyles({
     app: {
         alignItems: 'center',
-        // justifyContent: 'center',
     },
+    nowallet: {
+        maxWidth: '40%',
+        margin: '0 auto',
+    }
 }));
 
 function App() {
     // [<name of state>, <function to alter state>] = useState(<initialState>)
-    const [name, setName] = useState('example');
+    // const [name, setName] = useState('example');
     const classes = useStyles();
 
-    return (
-        <div className={classes.app}>
-            <Header />
-            <Body onClick={() => setName('Jerry')} />
-            <span>
-                {name}
-            </span>
-        </div>
-    );
+    if ((window as any).ethereum) {
+        return (
+            <div className={classes.app}>
+                <Header />
+                <Body onClick={() => alert('Hello!')} />
+            </div>
+        );
+    }
+    else {
+        return (
+            <Card className={classes.nowallet}>
+                <CardContent style={{ textAlign: 'center' }}>
+                    <Typography>No wallet detected.</Typography>
+                </CardContent>
+                <CardActions style={{ justifyContent: 'center' }}>
+                    <a href='https://metamask.io/download.html'>
+                        <Button variant='outlined'>Install Metamask</Button>
+                    </a>
+                </CardActions>
+            </Card>
+        )
+    }
 }
 
 export default App;
