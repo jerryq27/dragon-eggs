c24ec1443c7a8b2ce9d214594ea666cc3a25f9bd
Jerry Quintero <jerryq27@gmail.com>
2022-04-16 19:03:10 -0500

    Using generated contract from OpenZeppelin.

--- contracts/DragonEggs.sol
+++ contracts/DragonEggs.sol
@@ -1,27 +1,67 @@
 // SPDX-License-Identifier: MIT
-pragma solidity >=0.8.0;
+pragma solidity ^0.8.4;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
 
-contract DragonEggs is ERC721URIStorage {
-    uint256 public tokenCounter;
+contract DragonEggs is ERC721, ERC721URIStorage, Ownable {
+    using Counters for Counters.Counter;
 
-    constructor() ERC721("Dragon Eggs", "DEGG") {
-        tokenCounter = 0;
+    Counters.Counter private _tokenIdCounter;
+    mapping(string => uint8) existingURIs;
+
+    constructor() ERC721("Dragon Eggs", "DEGG") {}
+
+    function _baseURI() internal pure override returns (string memory) {
+        return "ipfs://";
     }
 
-    function mintToken(address user, string memory tokenURI)
+    function safeMint(address to, string memory uri) public onlyOwner {
+        uint256 tokenId = _tokenIdCounter.current();
+        _tokenIdCounter.increment();
+        _safeMint(to, tokenId);
+        _setTokenURI(tokenId, uri);
+    }
+
+    // The following functions are overrides required by Solidity.
+
+    function _burn(uint256 tokenId)
+        internal
+        override(ERC721, ERC721URIStorage)
+    {
+        super._burn(tokenId);
+    }
+
+    function tokenURI(uint256 tokenId)
         public
+        view
+        override(ERC721, ERC721URIStorage)
+        returns (string memory)
+    {
+        return super.tokenURI(tokenId);
+    }
+
+    function isURIOwned(string memory uri) public view returns (bool) {
+        return existingURIs[uri] == 1;
+    }
+
+    function payToMint(address buyer, string memory uri)
+        public
+        payable
         returns (uint256)
     {
-        uint256 newTokenId = tokenCounter;
+        require(existingURIs[uri] != 1, "That NFT has already been minted!");
+        require(msg.value >= 0.01 ether, "Insufficient funds.");
 
-        // ERC721 contract methods.
-        _safeMint(user, newTokenId);
-        _setTokenURI(newTokenId, tokenURI);
+        uint256 newEggId = _tokenIdCounter.current();
+        _tokenIdCounter.increment();
+        existingURIs[uri] = 1;
 
-        tokenCounter = tokenCounter + 1;
-        return newTokenId;
+        _mint(buyer, newEggId);
+        _setTokenURI(newEggId, uri);
+
+        return newEggId;
     }
 }
