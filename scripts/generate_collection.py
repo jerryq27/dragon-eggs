0b6836e951fe0cc5fe6c2ecbf5abade7169b0ae3
Jerry Quintero <jerryq27@gmail.com>
2022-05-01 20:59:40 -0500

    Generating unique names.
    Unique names for both collection and the file. Also replaced print statements with logs.

--- scripts/generate_collection.py
+++ scripts/generate_collection.py
@@ -1,10 +1,18 @@
 import os
 import random
+import logging
 from PIL import Image
 
 # 5 layers x 5 options (color, pattern, background, gloss position, (rare) has crack)
 # Use 5 (0-4) digits to determine which option to use
 # Make sure they are unique by adding them to a dictionary
+
+logging.basicConfig(
+    filename=os.path.join(os.getcwd(), 'dragoneggs.log'),
+    filemode='w',
+    level=logging.DEBUG,
+    format='%(name)s:%(levelname)s - %(message)s'
+)
 
 COLLECTION_SIZE = 10
 dragon_eggs = {}
@@ -13,21 +21,22 @@ egg_metadata = {
     '': '',
 }
 name_background = ['shadowy', 'mountainous', 'volcanic', 'sea', 'forest']
-name_color = ['light', 'fire', 'wind', 'water', 'dark']
-name_pattern = ['spiraly', 'wavy', 'swirled', 'striped', 'diamond']
+name_color = ['orange', 'red', 'green', 'blue', 'purple']
+name_pattern = ['spiral', 'wave', 'swirl', 'stripe', 'diamond']
+name_rare_trait = ['common', 'uncommon', 'rare', 'super-rare', 'ultra-rare']
 
 
 def generate_collection():
     # [0-4][0-4][0-4][0-4?]
-    print('Generating collection..')
+    logging.info('Generating collection..\n')
     for i in range(COLLECTION_SIZE):
-        print('Calculating unique sequence..')
+        logging.info('Calculating unique sequence..')
         sequence = calculate_sequence()
-        print('Getting image layers..')
+        logging.info('Getting image layers..')
         images = get_images(sequence)
-        print(f'Creating collectible #{i} [{sequence}]..')
+        logging.info(f'Creating collectible #{i + 1} [{sequence}]..')
         create_collectible(sequence, images)
-        print('Done!\n')
+        logging.info('Done!\n')
 
 
 def calculate_sequence():
@@ -58,12 +67,12 @@ def determine_rare_trait():
     # 25% chance of getting an egg with the rare trait: a crack.
     has_rare_trait_num = random.randrange(1, 101)
     if has_rare_trait_num <= 75:
-        print('No rare trait!')
+        logging.info('No rare trait!')
         return 0
 
     # It does get a rare trait, determine which pattern.
     pattern_num = random.randint(1, 101)
-    print(f'Rare trait determinator: {pattern_num}')
+    logging.info(f'Rare trait! Determinator: {pattern_num}')
     # More common pattern, bottom crack: 25% chance.
     if pattern_num <= 25:
         return 1
@@ -92,20 +101,41 @@ def get_images(sequence):
     return images if crack else images[:-1]
 
 
+def create_file_name(sequence):
+
+    background = name_background[int(sequence[0])-1]
+    color = name_color[int(sequence[1])-1]
+    pattern = name_pattern[int(sequence[2])-1]
+    rare_trait = name_rare_trait[int(sequence[3])-1]
+
+    file_name = '-'.join(
+        [background, color, pattern, 'egg', rare_trait]
+    ) + '.png'
+
+    collection_name = f'{background.title()} {color.title()} {pattern.title()} Egg ({rare_trait.title()})'
+
+    return (collection_name, file_name)
+
+
 def create_collectible(sequence, images):
-    collectibles_path = os.path.join(
-        os.getcwd(), 'metadata', 'img', 'collectibles'
-    )
-    if not os.path.exists(collectibles_path):
-        os.mkdir(collectibles_path)
+    # collectibles_path = os.path.join(
+    #     os.getcwd(), 'metadata', 'img', 'collectibles'
+    # )
+    # if not os.path.exists(collectibles_path):
+    #     os.mkdir(collectibles_path)
 
-    layers = [Image.open(x) for x in images]
+    # layers = [Image.open(x) for x in images]
 
-    collectible = layers[0]
-    for layer in layers[1:]:
-        collectible.paste(layer, (0, 0), layer)
+    # collectible = layers[0]
+    # for layer in layers[1:]:
+    #     collectible.paste(layer, (0, 0), layer)
 
-    collectible.save(os.path.join(collectibles_path, f'{sequence}.png'), 'PNG')
+    collection_name, file_name = create_file_name(sequence)
+    logging.info(f"{collection_name} '{file_name}'")
+    # if not os.path.exists(os.path.join(collectibles_path, file_name)):
+    #     collectible.save(os.path.join(collectibles_path, file_name), 'PNG')
+    # else:
+    #     logging.warning(f'Duplicate: {sequence}: {file_name}')
 
 
 generate_collection()
